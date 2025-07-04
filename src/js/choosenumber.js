27295b4d2147e9c989a4e5057607b7b05831935c
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-07-04 21:13:45 +0300

    css

---
+++ src/js/choosenumber.js
@@ -0,0 +1,6 @@
+// const firstNumber = prompt("Введи перше число");
+// const secondNumber = prompt("Введи second число");
+// const thirdNumber = prompt("Введи third число");
+// const allNumbers = [firstNumber, secondNumber, thirdNumber];
+// const maxNumber = Math.max(...allNumbers);
+// console.log(`The biggest number is: ${maxNumber}`);
