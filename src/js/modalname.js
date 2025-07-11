429482cc7f7dcfa092f8890ed89bfa62322fb833
VladDraga <draga260806@gmail.com>
2025-07-11 18:20:20 +0200

    css +-
    commit

---
+++ src/js/modalname.js
@@ -0,0 +1,5 @@
+// const modalBtn = document.querySelector('.modal__name__close');
+// const modal = document.querySelector(".modal__name")
+// modalBtn.addEventListener("click", () => {
+//     modal.style.display = 'none';
+// })
