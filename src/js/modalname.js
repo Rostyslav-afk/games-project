56d51b7499aeeb4a40660d68a17bca5df8b4dc89
VladDraga <draga260806@gmail.com>
2025-07-16 19:10:19 +0200

    jsjsjsmodal
    commit

--- src/js/modalname.js
+++ src/js/modalname.js
@@ -1,7 +1,7 @@
 const modalBtnNameClose = document.querySelector(".modal__name__close");
 const modalName = document.querySelector(".modal__name");
 modalBtnNameClose.addEventListener('click', () => {
-    modalName.style.display = 'none';
+    modalName.classList.add("hidden");
 });
 const input = document.getElementById("username");
 const saveButton = document.querySelector(".modal_name__button");
@@ -11,11 +11,34 @@ saveButton.addEventListener("click", function () {
   const newName = input.value.trim();
 
   if (newName !== "") {
-      headerText.textContent = `Вітаємо, ${newName}!`;
-      modalName.style.display = 'none';
+    headerText.textContent = `Вітаємо, ${newName}!`;
+    
+    modalName.classList.add("modal_move");
+
+    setTimeout(() => {
+      modalName.classList.add("hidden");
+    }, 1200);
   }
 });
 
+document.body.addEventListener("click", (event) => {
+  if (event.target === event.currentTarget) {
+    modalName.classList.add("modal_move");
+    setTimeout(() => {
+      modalName.classList.add("hidden");
+    }, 1200);
+  }
+
+});
+
+window.addEventListener("keydown", (event) => {
+  if (event.code === "Escape") {
+    modalName.classList.add("modal_move");
+    setTimeout(() => {
+      modalName.classList.add("hidden");
+    }, 1200);
+  }
+});
 
 
 
