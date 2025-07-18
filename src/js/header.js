7826101d1884fd378bb2a7a72562f9ebdd215aff
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-07-18 14:09:00 +0300

    check

--- src/js/header.js
+++ src/js/header.js
@@ -12,3 +12,25 @@ input.addEventListener("click", (event) => {
         headerLogo.src = "../img/photo/logo.webp";
     }
 });
+
+
+// HEADER MODAL
+
+const numbersButton = document.querySelector(".b1");
+const gamesButton = document.querySelector(".b2");
+const tutorButton = document.querySelector(".b3");
+
+const numbersSections = document.querySelector(".number");
+const gamesSections = document.querySelector(".game");
+const tutorSections = document.querySelector(".tutor");
+numbersSections.style.opacity = "1";
+gamesSections.style.opacity = "1";
+tutorSections.style.opacity = "1";
+
+numbersButton.addEventListener("click", (event) => {
+    numbersSections.style.opacity = "1";
+    gamesSections.style.opacity = "0";
+    tutorSections.style.opacity = "0";
+    console.log("end callback");
+    
+})
