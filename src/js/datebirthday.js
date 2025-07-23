939cd77d1b95d78836df55592e0f6fbc3998117b
DavydSt <145857522+DavydSt@users.noreply.github.com>
2025-07-23 18:32:16 +0200

    fixed

--- src/js/datebirthday.js
+++ src/js/datebirthday.js
@@ -1,8 +1,16 @@
 function day() {
-  const birthday = document.querySelector('.date-birthday__number').value;
-  const number = birthday / 4;
-  if (number % 1 === 0) {
-    const text = document.querySelector('.date-birthday__text');
+  const year = Number(document.querySelector('.date-birthday__number').value);
+  const text = document.querySelector('.date-birthday__text');
+
+  const isLeap = year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+
+  if (year === '' || Number(year) === 0) {
+    text.style.color = '#ff0000';
+    text.textContent = 'Будь ласка, введіть коректний рік ';
+    return;
+  }
+
+  if (isLeap) {
     text.style.color = '#039900';
     text.textContent = 'Ви народилися у високосний рік!';
   } else {
@@ -10,8 +18,10 @@ function day() {
     text.textContent = 'Ви народилися не у високосний рік!';
   }
 }
+
 const push = document.querySelector('.date-birthday__button');
 push.addEventListener('click', day);
+
 document
   .querySelector('.date-birthday__number')
   .addEventListener('keyup', function (event) {
