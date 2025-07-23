8c78bb81064814e9db96859ff442396fdde25233
DavydSt <145857522+DavydSt@users.noreply.github.com>
2025-07-23 22:42:42 +0200

    coment fixed

--- src/js/randomnumber.js
+++ src/js/randomnumber.js
@@ -1,36 +1,3 @@
-// function random() {
-//   let min = 1;
-//   let max = 10;
-//   let number = Math.floor(Math.random() * (max - min + 1)) + min;
-//   const userNum = document.querySelector('.random-number__number').value;
-//   const text = document.querySelector('.random-number__text');
-
-//   if (userNum === '' || userNum === 0) {
-//     text.style.color = '#900';
-//     text.textContent = 'Будь ласка, введіть число від 1 до 10.';
-//     return;
-//   }
-
-//   if (number === userNum) {
-//     text.style.color = '#039900';
-//     text.textContent = `Вітаю, ви вгадали число ${number}!`;
-//   } else {
-//     text.style.color = '#900';
-//     text.textContent = `Ви програли, комп’ютер загадав ${number}!`;
-//   }
-// }
-
-// const click = document.querySelector('.random-number__btn');
-// click.addEventListener('click', random);
-// document
-//   .querySelector('.random-number__number')
-//   .addEventListener('keyup', function (event) {
-//     if (event.key === 'Enter') {
-//       random();
-//     }
-//   });
-
-
 function random() {
   const min = 1;
   const max = 10;
